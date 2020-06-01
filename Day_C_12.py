c77eaf3e71551a2f0e1b4a0c805496bcbe710997
PKR-808 <39454090+PKR-808@users.noreply.github.com>
2020-06-01 15:31:03 +0545

    Day 12: Inheritance

---
+++ Day_C_12.py
@@ -0,0 +1,58 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self,fName, lName, idnum, scores):
+        super().__init__(fName,lName,idnum)
+        self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+   
+    def calculate(self):
+        totalscores = 0
+        for i in self.scores:
+            totalscores += i
+        a = totalscores/numScores
+        if a>=90:
+            return "O"
+        elif a>=80:
+            return "E"
+        elif a>=70:
+            return "A"
+        elif a>=55:
+            return "P"
+        elif a>=40:
+            return "D"
+        else:
+            return "T"
+
+
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
